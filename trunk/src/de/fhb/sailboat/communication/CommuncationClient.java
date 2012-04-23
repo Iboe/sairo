b5877d4a5f92581704540ad5c0433033e1097da6
fhb-sairo <fhb-sairo@2e2d5edc-86b9-48e7-b817-bf6befc431f3>
2012-04-23 18:34:48 +0000

    - user: kant - the CommunicationClient class is ready for now and a test of the communications component is imminent
    git-svn-id: http://svn.code.sf.net/p/segelboot/code@76 2e2d5edc-86b9-48e7-b817-bf6befc431f3

--- trunk/src/de/fhb/sailboat/communication/CommuncationClient.java
+++ trunk/src/de/fhb/sailboat/communication/CommuncationClient.java
@@ -3,7 +3,14 @@
  */
 package de.fhb.sailboat.communication;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
+import java.net.UnknownHostException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * to be commented
@@ -12,17 +19,82 @@ import java.net.Socket;
  */
 public class CommuncationClient extends CommunicationBase {
 
-	private Socket destination;
+	private static final Logger LOG = LoggerFactory.getLogger(CommuncationClient.class);
 	
-	public CommuncationClient(String destinationIP, int port){
+	private Socket destination;
+	private String destinationIP;
+	private int destinationPort;
+	
+	
+	
+	public CommuncationClient(String destIP, int port){
 		
+		destinationIP=destIP;
+		destinationPort=port;
+		destination=null;
+	}
+	
+	@Override
+	public boolean initialize(){
 		
+		boolean bInitialized=false;
+		
+		if(!isConnected()){
+			
+			try {
+				
+				destination=new Socket("destinationIP",destinationPort);
+				LOG.debug("Connecting to "+destinationIP+":"+destinationPort+"...");
+					
+				
+				setSender(new DataOutputStream(destination.getOutputStream()));
+				setReceiver(new DataInputStream(destination.getInputStream()));
+				bInitialized=super.initialize();
+				LOG.info("Initialized successful.");
+			}
+			catch (UnknownHostException e) {
+				
+				LOG.error("Invalid host - "+destinationIP+": "+e.getMessage());
+				
+				destination=null;
+				
+			} catch (IOException e) {
+				
+				LOG.error("Failed to initialize: "+e.getMessage());
+				destination=null;
+			}
+		}
+		
+		return bInitialized;
+	}
+	
+	@Override
+	public void shutdown(){
+		
+		LOG.debug("Shutting down...");
+		
+		setSender(null);
+		setReceiver(null);
+		if(destination != null){
+			
+			try {
+				destination.close();
+				LOG.debug("Remote connection closed.");	
+			} 
+			catch (IOException e) {
+				
+				LOG.error("Error closing remote connection: " +e.getMessage());
+				e.printStackTrace();
+			}
+		}
+		
+		super.shutdown();
 	}
 	
 	@Override
 	public boolean isConnected() {
-		// TODO Auto-generated method stub
-		return false;
+		
+		return destination != null && !(destination.isClosed() || destination.isInputShutdown() || destination.isOutputShutdown());
 	}
 
 }
