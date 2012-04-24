fb396bb371d64786cea100c1bb9764931472896b
fhb-sairo <fhb-sairo@2e2d5edc-86b9-48e7-b817-bf6befc431f3>
2012-04-24 19:06:41 +0000

    - user: kant - added javadoc for the TransmissionModule interface
    git-svn-id: http://svn.code.sf.net/p/segelboot/code@82 2e2d5edc-86b9-48e7-b817-bf6befc431f3

--- trunk/src/de/fhb/sailboat/communication/TransmissionModule.java
+++ trunk/src/de/fhb/sailboat/communication/TransmissionModule.java
@@ -8,14 +8,49 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 
 /**
- * to be commented
+ * Interface declaring a {@link TransmissionModule}. Clases implementing the {@link TransmissionModule} are responsible<br>
+ * for defining a serialization and deserialization algorithm for a certain type of data.<br>
+ * Transmission modules are to be registered at a {@link CommunicationBase} instance.<br>
+ * Since {@link TransmissionModule}s on the server and client usually have different tasks to perform,<br>
+ * there must be created a pair of {@link TransmissionModule} classes; one for the client and one for the server.<br>
+ * If just one {@link TransmissionModule} type were to be added at the server and the client, it would result in a cyclic<br>
+ * ping-pong like transmission, which is usually not intended. <br> 
+ * <b>In order to ensure a successful transmission, all {@link TransmissionModule} pairs must be registered at the<br>
+ * end-nodes (server and client) in the SAME order!!</b><br>
+ *   
  * @author Michael Kant
  *
  */
 public interface TransmissionModule {
 
+	/**
+	 * Called on a registered {@link TransmissionModule}, if the related {@link CommunicationBase} received a transmission<br>
+	 * that's intended for this {@link TransmissionModule}.<br>
+	 * The given {@link TransmissionModule} is supposed to decode the data on its own.
+	 * 
+	 * @param stream Stream that provides the data.
+	 * @throws IOException IOException that's back-propagated to the underlying {@link CommunicationBase}, if this {@link TransmissionModule} doesn't handle the exception itself.
+	 */
 	public void objectReceived(DataInputStream stream) throws IOException;
-	public void requestObject(DataOutputStream stream) throws IOException;
 	
+	/**
+	 * Called by the underlying {@link ModuleWorker}, if the {@link TransmissionModule}'s transmission interval has been reached
+	 * and a new object or bunch of data is supposed to be sent.<br>
+	 * The given {@link TransmissionModule} is supposed to encode the data on its own.<br>
+	 * Mind, this method is just called if the given transmission interval is greater than 0. (see {@link #getTransmissionInterval()})
+	 * 
+	 * @param stream Stream that takes the data.
+	 * @throws IOException IOException that's back-propagated to the underlying {@link ModuleWorker}, if this {@link TransmissionModule} doesn't handle the exception itself.
+	 */
+	public void requestObject(DataOutputStream stream) throws IOException;
+
+	/**
+	 * Defines the transmission interval of this {@link TransmissionModule}.<br>
+	 * The return value is interpreted as milliseconds.<br>
+	 * Setting a transmission interval of 0 or less will cause the {@link TransmissionModule} to be declared as passive.<br>
+	 * That means it can just receive incoming objects by {@link #objectReceived(DataInputStream)} because {@link #requestObject(DataOutputStream)} is never being called.
+	 * 
+	 * @return The transmission interval value.
+	 */
 	public int getTransmissionInterval();
 }
