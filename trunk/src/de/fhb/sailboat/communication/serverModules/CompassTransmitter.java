00efc096c7e5247e82d8a4e7587189a1c8eddf61
fhb-sairo <fhb-sairo@2e2d5edc-86b9-48e7-b817-bf6befc431f3>
2012-04-26 23:12:41 +0000

    -user: kant -added transmitter and receiver modules for GPS and Compass data
    git-svn-id: http://svn.code.sf.net/p/segelboot/code@93 2e2d5edc-86b9-48e7-b817-bf6befc431f3

---
+++ trunk/src/de/fhb/sailboat/communication/serverModules/CompassTransmitter.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package de.fhb.sailboat.communication.serverModules;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.fhb.sailboat.communication.CommunicationBase;
+import de.fhb.sailboat.communication.TransmissionModule;
+import de.fhb.sailboat.data.Compass;
+import de.fhb.sailboat.worldmodel.CompassModel;
+import de.fhb.sailboat.worldmodel.WorldModel;
+import de.fhb.sailboat.worldmodel.WorldModelImpl;
+
+/**
+ * Transmitter module for the {@link CompassModel}'s {@link Compass} data.<br>
+ * The yaw/azimuth a {@link Compass} object is being transformed into an integer value<br>
+ * and written to the output stream as compact index.<br>
+ * This is an active module with a transmission interval of 500ms.
+ * 
+ * @author Michael Kant
+ *
+ */
+public class CompassTransmitter implements TransmissionModule{
+
+	private static final Logger LOG = LoggerFactory.getLogger(CompassTransmitter.class);
+	
+	private Compass lastCompass;
+	private WorldModel worldModel;
+	
+	public CompassTransmitter() {
+	
+		lastCompass=null;
+		worldModel=WorldModelImpl.getInstance();
+	}
+	/* (non-Javadoc)
+	 * @see de.fhb.sailboat.communication.TransmissionModule#objectReceived(java.io.DataInputStream)
+	 */
+	@Override
+	public void objectReceived(DataInputStream stream) throws IOException {
+		
+		LOG.warn("Received an unexpected incoming transmission.");
+
+	}
+
+	/* (non-Javadoc)
+	 * @see de.fhb.sailboat.communication.TransmissionModule#skipNextCycle()
+	 */
+	@Override
+	public boolean skipNextCycle() {
+		
+		return lastCompass == worldModel.getCompassModel().getCompass();
+	}
+
+	/* (non-Javadoc)
+	 * @see de.fhb.sailboat.communication.TransmissionModule#requestObject(java.io.DataOutputStream)
+	 */
+	@Override
+	public void requestObject(DataOutputStream stream) throws IOException {
+		
+		int yaw=0;
+		Compass current=worldModel.getCompassModel().getCompass();
+		
+		
+		if(current != null){
+			
+			yaw=(int)(current.getAzimuth()*10);	
+		}
+		CommunicationBase.writeCompactIndex(stream, yaw);
+		
+		lastCompass=current;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.fhb.sailboat.communication.TransmissionModule#getTransmissionInterval()
+	 */
+	@Override
+	public int getTransmissionInterval() {
+		
+		return 500;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.fhb.sailboat.communication.TransmissionModule#getPriority()
+	 */
+	@Override
+	public int getPriority() {
+		
+		return 0;
+	}
+}
---
+++ trunk/src/de/fhb/sailboat/communication/serverModules/GPSTransmitter.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package de.fhb.sailboat.communication.serverModules;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.fhb.sailboat.communication.CommunicationBase;
+import de.fhb.sailboat.communication.TransmissionModule;
+import de.fhb.sailboat.data.GPS;
+import de.fhb.sailboat.worldmodel.GPSModel;
+import de.fhb.sailboat.worldmodel.WorldModel;
+import de.fhb.sailboat.worldmodel.WorldModelImpl;
+
+/**
+ * Transmitter module for the {@link GPSModel}'s {@link GPS} data.<br>
+ * The latitude and longitude of a {@link GPS} object is being transformed into integer values<br>
+ * and written to the output stream as compact indices.<br>
+ * This is an active module with a transmission interval of 1000ms.
+ * 
+ * @author Michael Kant
+ *
+ */
+public class GPSTransmitter implements TransmissionModule {
+
+	private static final Logger LOG = LoggerFactory.getLogger(GPSTransmitter.class);
+	
+	private GPS lastGPS;
+	private WorldModel worldModel;
+	
+	public GPSTransmitter() {
+	
+		lastGPS=null;
+		worldModel=WorldModelImpl.getInstance();
+	}
+	/* (non-Javadoc)
+	 * @see de.fhb.sailboat.communication.TransmissionModule#objectReceived(java.io.DataInputStream)
+	 */
+	@Override
+	public void objectReceived(DataInputStream stream) throws IOException {
+		
+		LOG.warn("Received an unexpected incoming transmission.");
+
+	}
+
+	/* (non-Javadoc)
+	 * @see de.fhb.sailboat.communication.TransmissionModule#skipNextCycle()
+	 */
+	@Override
+	public boolean skipNextCycle() {
+		
+		return lastGPS == worldModel.getGPSModel().getPosition();
+	}
+
+	/* (non-Javadoc)
+	 * @see de.fhb.sailboat.communication.TransmissionModule#requestObject(java.io.DataOutputStream)
+	 */
+	@Override
+	public void requestObject(DataOutputStream stream) throws IOException {
+		
+		int lon=0,lat=0;
+		GPS current=worldModel.getGPSModel().getPosition();
+		
+		if(current != null){
+			
+			lat=(int)(current.getLatitude()*10000);
+			lon=(int)(current.getLongitude()*10000);	
+		}
+		
+		CommunicationBase.writeCompactIndex(stream, lat);
+		CommunicationBase.writeCompactIndex(stream, lon);
+		
+		lastGPS=current;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.fhb.sailboat.communication.TransmissionModule#getTransmissionInterval()
+	 */
+	@Override
+	public int getTransmissionInterval() {
+		
+		return 1000;
+	}
+
+	/* (non-Javadoc)
+	 * @see de.fhb.sailboat.communication.TransmissionModule#getPriority()
+	 */
+	@Override
+	public int getPriority() {
+		
+		return 0;
+	}
+
+}
