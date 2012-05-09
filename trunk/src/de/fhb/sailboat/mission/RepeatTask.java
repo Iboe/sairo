ef82f2797132dfe0faf7da35dbf770d56ae1053f
fhb-sairo <fhb-sairo@2e2d5edc-86b9-48e7-b817-bf6befc431f3>
2012-05-09 15:37:21 +0000

    - user: hscheel - added comments
    git-svn-id: http://svn.code.sf.net/p/segelboot/code@130 2e2d5edc-86b9-48e7-b817-bf6befc431f3

--- trunk/src/de/fhb/sailboat/mission/RepeatTask.java
+++ trunk/src/de/fhb/sailboat/mission/RepeatTask.java
@@ -4,6 +4,14 @@ import java.util.List;
 
 import de.fhb.sailboat.data.GPS;
 
+/**
+ * Task for repeated execution of other tasks. The subtasks are executed in the
+ * order of their position in the list. Tasks can be repeated for a specified
+ * number of times or infinite.
+ * 
+ * @author hscheel
+ *
+ */
 public class RepeatTask implements Task {
 
 	private final List<Task> tasks;
@@ -12,8 +20,18 @@ public class RepeatTask implements Task {
 	private int currentTaskPos;
 	private int currentRepetition;
 	
+	/**
+	 * Creates a new instance with the given tasks, which are repeated n times,
+	 * where repetitions specifies n. A number smaller than 0 will result in
+	 * infinite repetition.
+	 * Will throw an IllegalArgumentException if the task list is null, empty or 
+	 * repetitions is 0.
+	 * 
+	 * @param tasks the tasks to be executed
+	 * @param repetitions repeats the task n times, or infinite if < 0
+	 */
 	public RepeatTask(List<Task> tasks, int repetitions) {
-		if (tasks == null || tasks.size() == 0 || repetitions == 0) {
+		if (tasks == null || tasks.isEmpty() || repetitions == 0) {
 			throw new IllegalArgumentException();
 		}
 			
@@ -26,7 +44,7 @@ public class RepeatTask implements Task {
 	@Override
 	public boolean isFinished(GPS position) {
 		if (currentTask.isFinished(position)) {
-			//change task if current one is finished
+			//change task if the current one is finished
 			currentTaskPos++;
 			currentTask = tasks.get(currentTaskPos);
 			if (currentTaskPos == tasks.size()) {
--- trunk/src/de/fhb/sailboat/mission/StopTask.java
+++ trunk/src/de/fhb/sailboat/mission/StopTask.java
@@ -2,14 +2,28 @@ package de.fhb.sailboat.mission;
 
 import de.fhb.sailboat.data.GPS;
 
+/**
+ * Task that causes the boat to stop. Will never finish.
+ * 
+ * @author hscheel
+ *
+ */
 public class StopTask implements Task{
 
 	private final boolean turnPropellorOff;
 	
+	/**
+	 * Creates a new instance and turns the propellor off.
+	 */
 	public StopTask() {
 		this(false);
 	}
 	
+	/**
+	 * Creates a new instance and turns the propellor off if specified.
+	 * 
+	 * @param turnPropellorOff specifies if the propellor shall be turned off
+	 */
 	public StopTask(boolean turnPropellorOff) {
 		this.turnPropellorOff = turnPropellorOff;
 	}
