3580d0802683fa9b20dd6d54d26fd3aac38c2f97
fhb-sairo <fhb-sairo@2e2d5edc-86b9-48e7-b817-bf6befc431f3>
2012-05-15 15:26:09 +0000

    - user: hscheel - created own package for pilot and own file for DriveAngleThread - created enum for driving angle modes - added comments - implemented holdAngleToWind
    git-svn-id: http://svn.code.sf.net/p/segelboot/code@132 2e2d5edc-86b9-48e7-b817-bf6befc431f3

---
+++ trunk/src/de/fhb/sailboat/control/pilot/PilotImpl.java
@@ -0,0 +1,61 @@
+package de.fhb.sailboat.control.pilot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.fhb.sailboat.serial.actuator.LocomotionSystem;
+
+/**
+ * Implementation for the Pilot interface.
+ * 
+ * @author Michael Kant
+ *
+ */
+public class PilotImpl implements Pilot {
+
+	private static final Logger LOG = LoggerFactory.getLogger(PilotImpl.class);
+	
+	private final LocomotionSystem locSystem;
+	
+	private DriveAngleThread driveAngleThread;
+	
+	public PilotImpl(LocomotionSystem ls){
+		driveAngleThread=null;
+		locSystem=ls;
+	}
+	
+	@Override
+	public void driveAngle(int angle) {
+		driveAngle(angle, DriveAngleMode.COMPASS);		
+	}
+
+	@Override
+	public void holdAngleToWind(int angle) {
+		driveAngle(angle, DriveAngleMode.WIND);
+	}
+
+	private void driveAngle(int angle, DriveAngleMode mode) {
+		if (driveAngleThread != null && driveAngleThread.isAlive()) {
+			driveAngleThread.driveAngle(angle, mode);
+		} else {
+			driveAngleThread = new DriveAngleThread(locSystem);
+			driveAngleThread.driveAngle(angle, mode);
+			driveAngleThread.start();
+		}
+	}
+	
+	@Override
+	public void setPropellor(int value) {
+		locSystem.setPropellor(value);
+	}
+
+	@Override
+	public void setRudder(int value) {
+		locSystem.setRudder(value);
+	}
+
+	@Override
+	public void setSail(int value) {
+		locSystem.setSail(value);
+	}
+}
