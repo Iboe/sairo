20a43401e7e8993849b6462c6fb24d0db084b038
fhb-sairo <fhb-sairo@2e2d5edc-86b9-48e7-b817-bf6befc431f3>
2012-05-29 16:41:23 +0000

    Patrick -reworked whole GUI to be more streamlined, flexible and organized -new GUI implementation separates interface and logic (GUInterface and GUILogic) -it is possible to create different interface- and logic-classes and connect them (other boats) -current commit is not (quite) complete
    git-svn-id: http://svn.code.sf.net/p/segelboot/code@184 2e2d5edc-86b9-48e7-b817-bf6befc431f3

---
+++ trunk/src/de/fhb/sailboat/gui/GUITest.java
@@ -0,0 +1,11 @@
+package de.fhb.sailboat.gui;
+
+public class GUITest {
+	
+	public static void main(String[] args) {
+        // TODO code application logic here
+        GUInterface view = new GUInterface();
+        view.setVisible(true);
+    }
+	
+}
