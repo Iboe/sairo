8a0c9718ea1e9b9227318e42284310d64dfe9fa6
djdealer <djdealer@2e2d5edc-86b9-48e7-b817-bf6befc431f3>
2012-06-20 12:17:56 +0000

    Mergerd the XbeeWifi into
    git-svn-id: http://svn.code.sf.net/p/segelboot/code@247 2e2d5edc-86b9-48e7-b817-bf6befc431f3

---
+++ trunk/src/de/fhb/sailboat/wificonnection/IwifiXbee.java
@@ -0,0 +1,31 @@
+package de.fhb.sailboat.wificonnection;
+
+import java.io.IOException;
+
+import com.rapplogic.xbee.api.XBeeException;
+
+public interface IwifiXbee {
+
+	/*
+	 * Receives data from another Xbee
+	 */
+	public void read();
+	
+	/*
+	 * To Start the Xbee Module
+	 */
+	public void initializeXbee();
+	
+	/*
+	 * gets the Xbee Response 
+	 * Null if nothing happend
+	 */
+	public String getResp();
+	
+	/*
+	 * Sends Data to other xbee 
+	 */
+	public void sendDataXbee(String data) throws XBeeException,
+	InterruptedException, IOException;
+	
+}
