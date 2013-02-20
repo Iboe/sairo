a520e5d0a180e15e40d7211f06a9c7f9f91549ad
fhb-sairo <fhb-sairo@2e2d5edc-86b9-48e7-b817-bf6befc431f3>
2013-02-20 16:54:45 +0000

    - user: kant - added javadoc comments to the class TaskSerializer
    git-svn-id: http://svn.code.sf.net/p/segelboot/code@407 2e2d5edc-86b9-48e7-b817-bf6befc431f3

--- trunk/src/de/fhb/sailboat/communication/mission/TaskSerializer.java
+++ trunk/src/de/fhb/sailboat/communication/mission/TaskSerializer.java
@@ -5,18 +5,19 @@ package de.fhb.sailboat.communication.mission;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import de.fhb.sailboat.control.pilot.Pilot;
-import de.fhb.sailboat.control.pilot.PilotImpl;
 import de.fhb.sailboat.mission.Task;
 
 /**
+ * Manages the serialization and deserialization of {@link Task}s. The supported {@link Task}s can be configured in the configuration file,<br>
+ * by defining mappings between the desired {@link Task}s and corresponding {@link SerializedTask}s for the de-/serialization.<br>
+ * Before adding new {@link Task}s to the mapping list, an, appropriate de-/serialization class must be defined, which implements {@link SerializedTask}. 
+ * 
  * @author Michael Kant
  *
  */
@@ -32,6 +33,10 @@ public class TaskSerializer {
 	private List< Class<? extends Task> > taskClasses;
 	private List< Class<? extends SerializedTask> > serializedTaskClasses;
 	
+	/**
+	 * Default constructor.<br>
+	 * Initializes the class lists with the {@link Task} - {@link SerializedTask} mappings of the config file.
+	 */
 	public TaskSerializer(){
 	
 		Class<? extends Task> taskClass=null;
@@ -75,6 +80,18 @@ public class TaskSerializer {
 		
 	}
 	
+	/**
+	 * Serializes a given {@link Task} and returns the serialized data as byte array.<br>
+	 * First it looks for a valid mapping between the given {@link Task} and a respective {@link SerializedTask}.<br>
+	 * If a mapping was found, an instance of the corresponding {@link SerializedTask} class will be created and the given {@link Task} object will be supplied.
+	 * A call of the method {@link SerializedTask#getSerializedData()} will instigate the serialization of the given {@link Task} object.
+	 * A leading byte will be added to the array, which describes the {@link Task} that was serialized.<br>
+	 * This information is important for the subsequent deserialization, so it's aware of, which kind of {@link Task} needs to be deserialized.<br>
+	 * If any of those steps fail, this method will return null.
+	 * 
+	 * @param t The task to be serialized.
+	 * @return The serialized data, describing the {@link Task}, or null if the procedure encountered a problem.
+	 */
 	public byte[] serializeTask(Task t){
 		
 		byte[] serializedData,data=null;
@@ -104,19 +121,24 @@ public class TaskSerializer {
 				catch (SecurityException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				} catch (NoSuchMethodException e) {
+				} 
+				catch (NoSuchMethodException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				} catch (IllegalArgumentException e) {
+				} 
+				catch (IllegalArgumentException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				} catch (InstantiationException e) {
+				} 
+				catch (InstantiationException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				} catch (IllegalAccessException e) {
+				} 
+				catch (IllegalAccessException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-				} catch (InvocationTargetException e) {
+				} 
+				catch (InvocationTargetException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
@@ -126,6 +148,16 @@ public class TaskSerializer {
 		return data;
 	}
 	
+	/**
+	 * Deserializes the given data and returns a {@link Task} object.<br>
+	 * First it reads the first byte of the data, which tells what {@link Task} was serialized. Then it looks for a valid mapping, based on the read byte value.<br>
+	 * If a mapping was found, an instance of the corresponding {@link SerializedTask} class will be created and the given data will be supplied.
+	 * A call of the method {@link SerializedTask#getTask()} will instigate the deserialization of the given data.
+	 * If any of those steps fail, this method will return null.
+	 * 
+	 * @param data The serialized task data
+	 * @return The Task, which was obtained out of the data, or null if the procedure encountered a problem.
+	 */
 	public Task deserializeTask(byte[] data){
 		
 		Task task=null;
@@ -148,7 +180,6 @@ public class TaskSerializer {
 				
 				try {
 					
-					//System.out.println("constructor:" +sTaskClass.getConstructors()[1].getParameterTypes()[0].getCanonicalName());
 					sTaskConstructor=sTaskClass.getConstructor(byte[].class);
 					sTask=sTaskConstructor.newInstance(serializedData);
 					
@@ -187,9 +218,7 @@ public class TaskSerializer {
 					e.printStackTrace();
 				}
 			}
-			
 		}
-		
 		return task;
 	}
 }
