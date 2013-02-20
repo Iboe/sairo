5bfc8639a106b9752f229563931470abd12f5166
fhb-sairo <fhb-sairo@2e2d5edc-86b9-48e7-b817-bf6befc431f3>
2013-02-20 13:38:01 +0000

    - user: kant - extended the CommModuleTest class with miscellaneous sub-tests and the mission transmission including tasks
    git-svn-id: http://svn.code.sf.net/p/segelboot/code@404 2e2d5edc-86b9-48e7-b817-bf6befc431f3

--- trunk/src/de/fhb/sailboat/test/CommModuleTest.java
+++ trunk/src/de/fhb/sailboat/test/CommModuleTest.java
@@ -1,18 +1,30 @@
 package de.fhb.sailboat.test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
 
 import de.fhb.sailboat.communication.CommunicationBase;
 import de.fhb.sailboat.communication.carrierAdapters.CommTCPClient;
 import de.fhb.sailboat.communication.carrierAdapters.CommTCPServer;
 import de.fhb.sailboat.communication.clientModules.GPSReceiver;
 import de.fhb.sailboat.communication.clientModules.MissionTransmitter;
+import de.fhb.sailboat.communication.mission.SerializedReachCircleTask;
+import de.fhb.sailboat.communication.mission.SerializedTask;
+import de.fhb.sailboat.communication.mission.SerializedTaskBase;
+import de.fhb.sailboat.communication.mission.TaskSerializer;
 import de.fhb.sailboat.communication.serverModules.GPSTransmitter;
 import de.fhb.sailboat.communication.serverModules.MissionReceiver;
 import de.fhb.sailboat.control.Planner;
+import de.fhb.sailboat.data.GPS;
+import de.fhb.sailboat.mission.HoldAngleToWindTask;
 import de.fhb.sailboat.mission.Mission;
 import de.fhb.sailboat.mission.MissionImpl;
 import de.fhb.sailboat.mission.PrimitiveCommandTask;
+import de.fhb.sailboat.mission.ReachCircleTask;
 import de.fhb.sailboat.mission.Task;
 import de.fhb.sailboat.test.Initializier.PropertiesInitializer;
 
@@ -46,20 +58,14 @@ public class CommModuleTest {
 			
 		}
 		else{
-			int[] arr1={1,4,7,8,4,3,5};
-			int[] arr2={0,0,0,0,0,0,0};
 			
-			for(int i=0;i<arr1.length;System.out.print(arr1[i++]+" "));
-			System.out.println();
-			for(int i=0;i<arr1.length;System.out.print(arr2[i++]+" "));
-			System.out.println();
-			for(int i=0;i<arr1.length;arr2[i]=arr1[i++]);
+			PropertiesInitializer propInit=new PropertiesInitializer();
+			propInit.initializeProperties();
 			
-			for(int i=0;i<arr1.length;System.out.print(arr1[i++]+" "));
-			System.out.println();
-			for(int i=0;i<arr1.length;System.out.print(arr2[i++]+" "));
-			System.out.println();
+			cmt=new CommModuleTest();
 			
+			//testSerialTasks();
+			cmt.testTaskSerializer();
 			
 			System.out.println("No mode specified!");
 		}
@@ -98,17 +104,106 @@ public class CommModuleTest {
 			e.printStackTrace();
 		}
 		System.out.println("Now instigating the mission transmission...");
-		plannerProxy.doMission(new MissionImpl());
+		//Task t=new ReachCircleTask(new GPS(52.34567,24.56789), 1337);
+		Mission m=new MissionImpl();
+		List<Task> taskList=new ArrayList<Task>();
+		taskList.add(new ReachCircleTask(new GPS(52.34567,24.56789), 1337));
+		taskList.add(new HoldAngleToWindTask(27));
+		taskList.add(new ReachCircleTask(new GPS(12.345,54.321), 2710));
+		taskList.add(new ReachCircleTask(new GPS(10.1101,13.337), 7353));
+		m.setTasks(taskList);
+		
+		for(Task t : taskList)
+			System.out.println("Attempting to transmit task: "+t);
+		
+		plannerProxy.doMission(m);
 		
 	}
 	
+	public void testSerialTasks(){
+		
+		ReachCircleTask t=new ReachCircleTask(new GPS(52.12453,24.84726),1337);
+		SerializedTask st=new SerializedReachCircleTask(t);
+		System.out.println("Task and serialized task instance created.");
+		System.out.println("TASK: "+t);
+		byte[] data=st.getSerializedData();
+		
+		if(data != null){
+			
+			System.out.println("-Serialized Data is("+data.length+"):");
+			for(int i=0;i<data.length;System.out.print((data[i++]&0xff)+" "));
+		}
+		System.out.println();
+		System.out.println("-Task valid: "+st.isValid());
+		
+		System.out.println("deserializing with correct data..");
+		//int checksum=SerializedTaskBase.generateChecksum(st.getSerializedData());
+		SerializedTask st2=new SerializedReachCircleTask(st.getSerializedData());
+		data=st2.getSerializedData();
+		System.out.println("-Serialized Data is("+data.length+"):");
+		//System.out.println("-checksum: "+checksum);
+		for(int i=0;i<data.length;System.out.print((data[i++]&0xff)+" "));
+		System.out.println();
+		System.out.println("-Task valid: "+st2.isValid());
+		System.out.println("-Task type is: "+st2.getTaskType().getSimpleName());
+		System.out.println("-Task: "+st2.getTask());
+		
+		System.out.println("now passing invalid data");
+		data=st.getSerializedData();
+		data[2]=27;
+		SerializedTask st3=new SerializedReachCircleTask(data);
+		data=st3.getSerializedData();
+		System.out.println("-Serialized Data is("+data.length+"):");
+		for(int i=0;i<data.length;System.out.print((data[i++]&0xff)+" "));
+		System.out.println("-Task valid: "+st3.isValid());
+		System.out.println("-Task type is: "+st3.getTaskType().getSimpleName());
+		System.out.println("-Task: "+st3.getTask());
+	}
+	
+	public void testTaskSerializer(){
+		
+		TaskSerializer ts;
+		Task task=new ReachCircleTask(new GPS(52.123456, 24.56789), 1337);
+		Task dTask;
+		byte[] serializedData;
+		
+		System.out.println("Given task: "+task);
+		ts=new TaskSerializer();
+		
+		System.out.println("Serializing..");
+		serializedData=ts.serializeTask(task);
+		
+		if(serializedData != null){
+			
+			System.out.print("Task serialized: ");
+			for(int i=0;i<serializedData.length;System.out.print((serializedData[i++]&0xff)+" "));
+			System.out.println();
+			
+			System.out.println("De-Serializing correct data..");
+			
+			dTask=ts.deserializeTask(serializedData);
+			
+			if(dTask != null)
+				System.out.println("DeserializedTask: "+dTask);
+			
+			System.out.println("De-Serializing incorrect data..");
+			
+			serializedData[4]=27;
+			dTask=ts.deserializeTask(serializedData);
+			
+			if(dTask != null)
+				System.out.println("DeserializedTask: "+dTask);
+			
+		}
+	}
 
+	
 	public static class PlannerDummy implements Planner {
 
 		@Override
 		public void doMission(Mission mission) {
 			
-			System.out.println("Received Mission:");
+			System.out.println("Received Mission("+mission.getTasks().size()+" Tasks):");
 			for(Task t:mission.getTasks())
 				System.out.println("Task: " + t);
 		}
