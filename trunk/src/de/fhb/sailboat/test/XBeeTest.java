6f31a6b4bfb8a0fe4daec9107ccbe3ad92f2c6c4
fhb-sairo <fhb-sairo@2e2d5edc-86b9-48e7-b817-bf6befc431f3>
2013-02-21 16:07:56 +0000

    - user: kant - altered the XBeeTest to check out the Input and Output forwarders for XBee
    git-svn-id: http://svn.code.sf.net/p/segelboot/code@410 2e2d5edc-86b9-48e7-b817-bf6befc431f3

--- trunk/src/de/fhb/sailboat/test/XBeeTest.java
+++ trunk/src/de/fhb/sailboat/test/XBeeTest.java
@@ -5,8 +5,11 @@ import java.io.InputStream;
 import java.util.Properties;
 import java.util.Set;
 
+import com.rapplogic.xbee.api.XBee;
 import com.rapplogic.xbee.api.XBeeException;
 
+import de.fhb.sailboat.communication.carrierAdapters.xBee.XBeeInputForwarder;
+import de.fhb.sailboat.communication.carrierAdapters.xBee.XBeeOutputForwarder;
 import de.fhb.sailboat.test.Initializier.PropertiesInitializer;
 import de.fhb.sailboat.wificonnection.IwifiXbee;
 import de.fhb.sailboat.wificonnection.WifiXbee;
@@ -21,8 +24,67 @@ public class XBeeTest {
 	 * @throws XBeeException
 	 * @throws IOException 
 	 */
-	public static void main(String[] args) throws XBeeException,
-			InterruptedException, IOException {
+	public static void main(String[] args){
+		
+		XBeeOutputForwarderTest();
+	}
+	
+	public static void XBeeOutputForwarderTest(){
+		
+		XBee xb=new XBee();
+		try {
+			xb.open("COM6", 9600);
+		
+			XBeeOutputForwarder out=new XBeeOutputForwarder(xb, "0 13 a2 0 40 33 f1 fb");
+			
+			String str="Hallo  welt!!";
+			
+			try {    
+				
+				System.out.println("Writing: "+str);
+				for(char c: str.toCharArray())			
+					out.write(c);
+				
+				System.out.println("Flushing..");
+				out.flush();
+			} 
+			catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (XBeeException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+	
+
+	public static void XBeeInputForwarderTest(){
+		
+		XBee xb=new XBee();
+		try {
+			xb.open("COM6", 9600);
+		
+			XBeeInputForwarder in=new XBeeInputForwarder(xb);
+			
+			System.out.println("Reading attempt...");
+			
+			int b=0;
+			while((b=in.read()) != -1)
+				System.out.println("RECEIVED: "+(char)b+"("+b+")");
+		
+			
+		} catch (XBeeException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void xBeeAPITest() throws XBeeException,InterruptedException, IOException{
+		
 		PropertiesInitializer propsInit = new PropertiesInitializer();
 		propsInit.initializeProperties();
 		IwifiXbee xbee = new WifiXbee();
