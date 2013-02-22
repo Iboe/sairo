5fc16490a69601529e3112b0933d1a85812ae4f5
fhb-sairo <fhb-sairo@2e2d5edc-86b9-48e7-b817-bf6befc431f3>
2013-02-22 15:22:29 +0000

    - user: kant - added the mission transmitter module to the GUI test class - changed the gui to start a communication server endpoint instead of a client endpoint
    git-svn-id: http://svn.code.sf.net/p/segelboot/code@416 2e2d5edc-86b9-48e7-b817-bf6befc431f3

--- trunk/src/de/fhb/sailboat/test/UferGUITest.java
+++ trunk/src/de/fhb/sailboat/test/UferGUITest.java
@@ -4,10 +4,14 @@
 package de.fhb.sailboat.test;
 
 import de.fhb.sailboat.communication.carrierAdapters.CommTCPClient;
+import de.fhb.sailboat.communication.carrierAdapters.CommTCPServer;
 import de.fhb.sailboat.communication.CommunicationBase;
+import de.fhb.sailboat.communication.TransmissionModule;
 import de.fhb.sailboat.communication.clientModules.CompassReceiver;
 import de.fhb.sailboat.communication.clientModules.GPSReceiver;
+import de.fhb.sailboat.communication.clientModules.MissionTransmitter;
 import de.fhb.sailboat.communication.clientModules.WindReceiver;
+import de.fhb.sailboat.control.Planner;
 import de.fhb.sailboat.gui.GUI;
 import de.fhb.sailboat.test.Initializier.PropertiesInitializer;
 import de.fhb.sailboat.ufer.prototyp.View;
@@ -27,15 +31,20 @@ public class UferGUITest {
 		PropertiesInitializer propsInit = new PropertiesInitializer();
 		propsInit.initializeProperties();
 		
-		CommunicationBase client=new CommTCPClient("127.0.0.1", 6699);
+		//CommunicationBase client=new CommTCPClient("176.16.18.18", 6699);
+		CommunicationBase client=new CommTCPServer(6699);
+		TransmissionModule mTrans=new MissionTransmitter(client);
+		
 		client.registerModule(new GPSReceiver());
 		client.registerModule(new CompassReceiver());
 		client.registerModule(new WindReceiver());
+		client.registerModule(mTrans);
+		
 		
 		if(!client.initialize())
 			System.out.println("Error connecting to BOAT!");
 		
-		GUI view=new GUI(null);
+		GUI view=new GUI((Planner)mTrans);
 		view.setVisible(true);
 
 	}
