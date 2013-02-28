42ae02e27cc9d9aab2272d63fc0d3e89ab7fabe5
fhb-sairo <fhb-sairo@2e2d5edc-86b9-48e7-b817-bf6befc431f3>
2013-02-28 14:25:42 +0000

    - user: hscheel - created package-info for the start package
    git-svn-id: http://svn.code.sf.net/p/segelboot/code@434 2e2d5edc-86b9-48e7-b817-bf6befc431f3

---
+++ trunk/src/de/fhb/sailboat/start/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains the classes for starting the application. The {@link de.fhb.sailboat.start.Initializier} is the
+ * normal starting class. The {@link de.fhb.sailboat.start.TestInitializier} is used for testing and debugging 
+ * without accessing the real sensors and actuators. Both classes contain a main method for starting the 
+ * application.
+ */
+package de.fhb.sailboat.start;
