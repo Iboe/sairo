fb683b137e04ed6789784cae02e67bcbf2e08e51
fhb-sairo <fhb-sairo@2e2d5edc-86b9-48e7-b817-bf6befc431f3>
2013-02-28 12:32:02 +0000

    - user: kant - documented locomoation system - fixed package description of data
    git-svn-id: http://svn.code.sf.net/p/segelboot/code@431 2e2d5edc-86b9-48e7-b817-bf6befc431f3

--- trunk/src/de/fhb/sailboat/data/package-info.java
+++ trunk/src/de/fhb/sailboat/data/package-info.java
@@ -2,7 +2,7 @@
  * 
  */
 /**
- * Contains all classes that serve as container for a specific set of data, like compass, GPS or wind data.<br>
+ * Contains all classes that serve as container for a specific set of data, like Compass, GPS or Wind data.<br>
  * Objects of those classes should be read-only, after they were instanced in order to avoid side effects when sharing their references.<br>   
  * They're used within the central world model to share data between all components of the application.
  * @see {@link de.fhb.sailboat.worldmodel.WorldModel}
--- trunk/src/de/fhb/sailboat/serial/actuator/LocomotionSystem.java
+++ trunk/src/de/fhb/sailboat/serial/actuator/LocomotionSystem.java
@@ -4,7 +4,9 @@
 package de.fhb.sailboat.serial.actuator;
 
 /**
- * @author KI-Lab
+ * The interface for issuing direct control commands to the underlying actuators.
+ * 
+ * @author Helge Scheel, Michael Kant
  *
  */
 public interface LocomotionSystem {
@@ -24,44 +26,90 @@ public interface LocomotionSystem {
 	public static final int PROPELLOR_NORMAL = Integer.parseInt(System.getProperty(AKSENLocomotion.class.getSimpleName() + ".PROPELLOR_NORMAL"));
 	public static final int PROPELLOR_MAX = Integer.parseInt(System.getProperty(AKSENLocomotion.class.getSimpleName() + ".PROPELLOR_MAX"));
 
+	/**
+	 * Returns the execution status for the last control command that was issued. 
+	 * @return The execution status for the last control command that was issued.
+	 */
 	public int getStatus();
 
 	
-	/**
+	/*
 	 * relative Position
 	 * Port 0 => servo sail
 	 * Range: 31 .. 114
 	 * Neutral: 73
 	 * TODO normalize -41 .. 0 .. +41 => -100 .. 0 .. +100
-	 */	
+	 */
+	/**
+	 * Sets the latitude of the sail.<br>
+	 * Ranges:<br>
+	 * -sail closed: 31
+	 * -sail opened: 114
+	 * @param angle The latitude to set.
+	 */
 	public void setSail(int angle);
 
-	/**
+	/*
 	 * relative Position
 	 * Port 1 => servo rudder
 	 * Range: 34 .. 108
 	 * Neutral: 68
 	 * TODO normalize -36 .. 0 .. +36 => -100 .. 0 .. +100
 	 */
+	/**
+	 * Sets the position of the rudder.<br>
+	 * Ranges:<br>
+	 * -rudder left:   34
+	 * -rudder center: 68
+	 * -rudder right:  108
+	 * 
+	 * @param angle The rudder position to set.
+	 */
 	public void setRudder(int angle);
 	
-	/**
+	/*
 	 * absolute Position
 	 * Port 3 => servo propellor ("Flautenschieber")
 	 * Range: 31 .. 114
 	 * Neutral: 73
 	 * TODO normalize 0 .. 82 => -100 .. 0 .. +100
 	 */	
+	/**
+	 * Sets the rotation direction and speed of the propeller.<br>
+	 * Ranges:<br>
+	 * -max. counter-clockwise: 31
+	 * -idle/no rotation:       73
+	 * -max. clockwise:         114 
+	 * 
+	 * @param angle The propeller direction and speed to set.
+	 */
 	public void setPropellor(int angle);
 	
 	/**
-	 * TODO Reset-Method on AKSEN-BOARD?
+	 * Resetting all actuators to their initial position.
 	 */
 	public void reset();
+	
+	/**
+	 * Resetting the rudder to its initial position.
+	 */
 	public void resetRudder();
+	
+	/**
+	 * Resetting the sail to its initial position.
+	 */
 	public void resetSail();
+	
+	/**
+	 * Resetting the propeller to its initial position.
+	 */
 	public void resetPropellor();
+	
+	/**
+	 * Getting the battery state, if the implementation supports that.
+	 */
 	public int getBatteryState();
+	
 	public void setDebug(boolean debug);
 	public long getWait_Sleep();
 	public void setWait_Sleep(int wait_sleep);
