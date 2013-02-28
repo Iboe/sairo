9ae4d7088ce4c42cf5e6aa1217b37149c10af1de
fhb-sairo <fhb-sairo@2e2d5edc-86b9-48e7-b817-bf6befc431f3>
2013-02-28 14:41:04 +0000

    - user: kant - added package info for test
    git-svn-id: http://svn.code.sf.net/p/segelboot/code@435 2e2d5edc-86b9-48e7-b817-bf6befc431f3

---
+++ trunk/src/de/fhb/sailboat/test/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains miscellaneous start classes, which start indivitual components of the system wtihout starting the whole application.  
+ * 
+ * @author Team Sairo
+ *
+ */
+package de.fhb.sailboat.test;
