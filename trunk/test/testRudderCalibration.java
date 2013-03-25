797d12a9a392ecb89c11296c460c4eddeb96cd36
fhb-sairo <fhb-sairo@2e2d5edc-86b9-48e7-b817-bf6befc431f3>
2013-03-25 09:24:01 +0000

    git-svn-id: http://svn.code.sf.net/p/segelboot/code@480 2e2d5edc-86b9-48e7-b817-bf6befc431f3

---
+++ trunk/test/testRudderCalibration.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.SimpleLayout;
+
+import de.fhb.sailboat.serial.actuator.AKSENLocomotion;
+import de.fhb.sailboat.start.PropertiesInitializer;
+
+
+public class testRudderCalibration {
+
+	private final String TAG = this.getClass().getSimpleName() + " - ";
+	
+	private final String LogFile="logFile.txt";
+	
+	private final Logger log = Logger.getLogger("org.apache");
+	private final Level logLevel = Level.DEBUG;
+	
+	int RUDDER_LEFT = 34;
+	int RUDDER_NORMAL = 68;
+	int RUDDER_RIGHT = 108;
+	
+	private AKSENLocomotion aksen;
+	
+	public testRudderCalibration(){
+		init();
+		log.debug(TAG+"start test for testing rudder calibration...");
+		test();
+	}
+	
+	public void init(){
+		try {
+			log.setLevel(logLevel);
+			SimpleLayout layout = new SimpleLayout();
+			ConsoleAppender consoleAppender = new ConsoleAppender(layout);
+			FileAppender fileAppender = new FileAppender(layout,LogFile);
+			log.addAppender(consoleAppender);
+			log.addAppender(fileAppender);
+			log.info(TAG + "logger state: " + log.getLevel().toString());
+			PropertiesInitializer propsInit = new PropertiesInitializer();
+			propsInit.initializeProperties();
+			aksen = new AKSENLocomotion(true);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void test(){
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		log.debug(TAG + "setting rudder to maximum calibrated right position");
+		aksen.setRudder(RUDDER_RIGHT);
+		System.out.println("Press Enter to setting rudder to calibrated middle position");
+		try {
+			br.readLine();
+			aksen.setRudder(RUDDER_NORMAL);
+			System.out.println("Press Enter to setting rudder to maximum calibrated left position");
+			br.readLine();
+			aksen.setRudder(RUDDER_LEFT);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
--- trunk/test/testStart.java
+++ trunk/test/testStart.java
@@ -2,7 +2,8 @@
 public class testStart {
 
 	public static void main(String arfs[]){
-		testAksenLocomotion test = new testAksenLocomotion();
+		//testAksenLocomotion test = new testAksenLocomotion();
+		testRudderCalibration test = new testRudderCalibration();
 	}
 	
 }
