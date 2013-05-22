32a5350bdc3f0a11166fd0b36e3bfc3f11c5cbb1
fhb-sairo <fhb-sairo@2e2d5edc-86b9-48e7-b817-bf6befc431f3>
2013-05-22 13:52:59 +0000

    Klay: Nachtrag, unversioned File added, sorry
    git-svn-id: http://svn.code.sf.net/p/segelboot/code@583 2e2d5edc-86b9-48e7-b817-bf6befc431f3

---
+++ trunk/src/de/fhb/sailboat/mission/MissionVO.java
@@ -0,0 +1,37 @@
+package de.fhb.sailboat.mission;
+
+import java.util.List;
+
+/**
+ * Simple implementation of a mission.
+ * 
+ * @author hscheel
+ *
+ */
+public class MissionVO {
+
+	private List<Task> tasks;
+
+
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
+	}
+
+
+	public List<Task> getTasks() {
+		return tasks;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuffer buffer = new StringBuffer();
+		
+		buffer.append("MissionImpl [");
+		for (Task task : tasks) {
+			buffer.append(task.getClass().getSimpleName()).append(", ");
+		}
+		buffer.append("]");
+		
+		return buffer.toString();
+	}
+}
